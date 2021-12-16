8180a966e07fb19f9bc3691c52268957ccae2afc
jackkitte <kakeru.tamashiro@revcomm.co.jp>
2021-12-17 04:06:28 +0900

    usersモデルの作成とテスト1

--- backend/app/models/core.py
+++ backend/app/models/core.py
@@ -1,8 +1,23 @@
-from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+from zoneinfo import ZoneInfo
+
+from pydantic import BaseModel, validator
+
+JST = ZoneInfo("Asia/Tokyo")
 
 
 class CoreModel(BaseModel):
     pass
+
+
+class DateTimeModelMixin(BaseModel):
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+    @validator("created_at", "updated_at", pre=True)
+    def default_datetime(cls, value: datetime) -> datetime:
+        return value or datetime.now(JST)
 
 
 class IDModelMixin(BaseModel):
