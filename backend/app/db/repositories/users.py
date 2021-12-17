8babbf7e2b5d10efc859a0a05318d1a8d26d6c5d
jackkitte <kakeru.tamashiro@revcomm.co.jp>
2021-12-17 19:18:37 +0900

    パスワードのハッシュ化

--- backend/app/db/repositories/users.py
+++ backend/app/db/repositories/users.py
@@ -1,5 +1,7 @@
 from app.db.repositories.base import BaseRepository
 from app.models.user import UserCreate, UserInDB
+from app.services import auth_service
+from databases import Database
 from fastapi import HTTPException, status
 from pydantic import EmailStr
 
@@ -35,6 +37,10 @@ REGISTER_NEW_USER_QUERY = """
 
 
 class UsersRepository(BaseRepository):
+    def __init__(self, db: Database) -> None:
+        super().__init__(db)
+        self.auth_service = auth_service
+
     async def get_user_by_email(self, *, email: EmailStr) -> UserInDB:
         user_record = await self.db.fetch_one(
             query=GET_USER_BY_EMAIL_QUERY, values={"email": email}
@@ -63,7 +69,11 @@ class UsersRepository(BaseRepository):
                 detail="このユーザー名はすでに登録されています",
             )
 
+        user_password_update = self.auth_service.create_salt_and_hashed_password(
+            plaintext_password=new_user.password
+        )
+        new_user_params = new_user.copy(update=user_password_update.dict())
         created_user = await self.db.fetch_one(
-            query=REGISTER_NEW_USER_QUERY, values={**new_user.dict(), "salt": "123"}
+            query=REGISTER_NEW_USER_QUERY, values=new_user_params.dict()
         )
         return UserInDB(**created_user)
